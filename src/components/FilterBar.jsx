c6647ab7029026a4157d79c7621c003c761589f8
Luka Savić <lukasavic18@gmail.com>
2026-04-09 17:18:15 +0200

    final linter errors fixed

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -97,10 +97,7 @@ export default function FilterBar({
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d={
-              "M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0" +
-              "0010.607 10.607z"
-            }
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
         <input
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -207,10 +207,7 @@ function InsightMock() {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d={
-              "M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0" +
-              "0010.607 10.607z"
-            }
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
           />
         </svg>
         <span className="truncate">Search tasks…</span>
